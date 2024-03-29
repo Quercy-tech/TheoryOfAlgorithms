d164c09b1910f68a30f5e208e29b12cb591023eb
Alex <alexghezha@gmail.com>
2024-03-29 14:29:20 +0200

    Laba 4 via Queue

---
+++ Laba4/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿
+namespace Laba4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(57, 260);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(240, 147);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Organize (using array)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Noto Serif Georgian", 26F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(824, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 71);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Exam";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(42, 526);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(255, 148);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Organize (using pointers)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(1040, 274);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(459, 400);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(379, 260);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(419, 414);
+            this.richTextBox2.TabIndex = 6;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Noto Serif Georgian", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(524, 185);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 43);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Students";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Noto Serif Georgian", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(1250, 185);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 43);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Result";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Noto Serif Georgian", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(51, 195);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(252, 33);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "What will you choose?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1659, 963);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Laba4/Form1.cs
@@ -0,0 +1,353 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Laba4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            richTextBox2.Text = "1,3";
+        }
+
+        private List<int> studentsList = new List<int>() { 3, 2, 1 };
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Clear the existing list in case of multiple modifications
+            studentsList.Clear();
+
+            // Get the text from the textbox
+            string textBoxContent = richTextBox2.Text;
+
+            // Split the text by comma or any other delimiter you prefer
+            string[] studentNumbers = textBoxContent.Split(',');
+
+            // Try converting each string to int and add to the list
+            foreach (string number in studentNumbers)
+            {
+                int studentNumber;
+                if (int.TryParse(number, out studentNumber))
+                {
+                    studentsList.Add(studentNumber);
+                }
+                else
+                {
+                    richTextBox2.Text = "";
+                    MessageBox.Show("Enter valid input!");
+                    return;
+
+                }
+            }
+
+
+
+            // Sample usage
+
+            int queueType = 0; // FIFO queue
+            int studentsNumber = studentsList.Count;
+
+            Algorithm algorithm = new Algorithm();
+            List<string> result = algorithm.TakingExam(studentsList, queueType, studentsNumber);
+            foreach (string line in result)
+            {
+                richTextBox1.Text += line + "\n";
+            }
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Clear the existing list in case of multiple modifications
+            studentsList.Clear();
+
+            // Get the text from the textbox
+            string textBoxContent = richTextBox2.Text;
+
+            // Split the text by comma or any other delimiter you prefer
+            string[] studentNumbers = textBoxContent.Split(',');
+
+            // Try converting each string to int and add to the list
+            foreach (string number in studentNumbers)
+            {
+                int studentNumber;
+                if (int.TryParse(number, out studentNumber))
+                {
+                    studentsList.Add(studentNumber);
+                }
+                else
+                {
+                    richTextBox2.Text = "";
+                    MessageBox.Show("Enter valid input!");
+                    return;
+
+                }
+            }
+
+
+
+            // Sample usage
+
+            int queueType = 1; // FIFO queue
+            int studentsNumber = studentsList.Count;
+
+            Algorithm algorithm = new Algorithm();
+            List<string> result = algorithm.TakingExam(studentsList, queueType, studentsNumber);
+            foreach (string line in result)
+            {
+                richTextBox1.Text += line + "\n";
+            }
+
+
+
+        }
+
+        class Algorithm
+        {
+            public List<string> TakingExam(List<int> studentsList, int queueType, int studentsNumber)
+            {
+                Queue queue = new Queue(studentsNumber); //Initialising
+                PointedQueue pointedQueue = new PointedQueue();
+                List<string> text = new List<string>();
+                Stopwatch stopwatch = new Stopwatch();
+                TimeSpan timespan = new TimeSpan();
+
+                int charNumber = 0; //Outputing the queue
+                foreach (int student in studentsList)
+                {
+                    foreach (char ch in student.ToString())
+                    {
+                        charNumber++; ;
+                    }
+                    charNumber++; ;
+                }
+                char[] queueLineList = new char[charNumber];
+                int i = 0;
+                foreach (int student in studentsList)
+                {
+                    foreach (char ch in student.ToString())
+                    {
+                        queueLineList[i] = ch;
+                        i++;
+                    }
+                    queueLineList[i] = '\t';
+                    i++;
+                }
+                String queueLine = new String(queueLineList);
+                text.Add(queueLine);
+
+                try //Do the task and count the time
+                {
+                    FillingQueue();
+                    OutputExam();
+                    text.Add("-----Break-----");
+                    FillingQueue();
+                    OutputExam(true);
+                }
+                catch (Exception ex) //Stop the program if necessary
+                {
+                    text.Add(ex.Message);
+                    return text;
+                }
+
+
+                return text;
+
+
+
+
+
+                void FillingQueue()
+                {
+                    stopwatch.Start();
+                    foreach (int student in studentsList)
+                    {
+                        if (queueType == 1)
+                        {
+                            queue.Enqueue(student);
+                        }
+                        else
+                        {
+                            pointedQueue.Enqueue(student);
+                        }
+                    }
+                    stopwatch.Stop();
+                    timespan = stopwatch.Elapsed;
+                    stopwatch.Reset();
+                    text.Add("Time elapsed while enqueueing (ms):" + (double)timespan.Ticks / 10000);
+                }
+
+                
+                
+                void OutputExam(bool practise = false)
+                {
+                    
+                    stopwatch.Start();
+
+                    int taskNumber = 1;
+                    string line = "";
+
+                    int currentStudent = (queueType == 1) ? queue.Dequeue() : pointedQueue.Dequeue();
+                    while ((queueType == 1) ? !queue.IsEmpty() : !pointedQueue.IsEmpty())
+                    {
+                        taskNumber += 1;
+                       
+                        line = (practise) ? "Practical work №" + (taskNumber - 1) + " and №" + (taskNumber) + " for" :
+                            "Theoretical survey №" + (taskNumber - 1) + " and №" + (taskNumber) + " for";
+                        
+
+                        int nextStudent = (queueType == 1) ? queue.Dequeue() : pointedQueue.Dequeue();
+                        text.Add(line + " " + currentStudent);
+                        text.Add("Transfering part of task №" + taskNumber + " for " + nextStudent);
+                        currentStudent = nextStudent;
+                    }
+                    taskNumber += 1;
+                    line = (practise) ? "Practical work №" + (taskNumber - 1) + " and №" + (taskNumber) + " for" :
+                            "Theoretical survey №" + (taskNumber - 1) + " and №" + (taskNumber) + " for";
+                    text.Add(line + " " + currentStudent);
+                    stopwatch.Stop();
+                    timespan = stopwatch.Elapsed;
+                    stopwatch.Reset();
+                    text.Add("Time elapsed while dequeueing (ms):" + (double)timespan.Ticks / 10000);
+                }
+
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+
+    class Queue
+    {
+        int[] array;
+        int head;
+        int tail;
+        int size;
+        int capacity;
+        public Queue(int capacity)
+        {
+            this.capacity = capacity;
+            this.array = new int[capacity];
+            this.head = 0;
+            this.tail = -1;
+            this.size = 0;
+        }
+        public void Enqueue(int item)
+        {
+            if (this.size == this.capacity)
+            {
+                throw new Exception("Queue overflow");
+            }
+            this.tail++;
+            if (this.tail == this.capacity)
+            {
+                this.tail = 0;
+            }
+            this.array[this.tail] = item;
+            this.size++;
+        }
+        public int Dequeue()
+        {
+            if (this.size == 0)
+            {
+                throw new Exception("Queue is empty");
+            }
+            int item = this.array[this.head];
+            this.head++;
+            if (this.head == this.capacity)
+            {
+                this.head = 0;
+            }
+            this.size--;
+            return item;
+        }
+        public bool IsEmpty()
+        {
+            return this.size == 0;
+        }
+
+    }
+
+
+
+
+    class PointedQueue
+    {
+        class QueueItem
+        {
+            public int item;
+            public QueueItem next;
+            public QueueItem(int item)
+            {
+                this.item = item;
+            }
+        }
+        QueueItem head;
+        QueueItem tail;
+
+        public void Enqueue(int item)
+        {
+            QueueItem newItem = new QueueItem(item);
+            if (this.tail == null)
+            {
+                this.head = newItem;
+            }
+            else
+            {
+                this.tail.next = newItem;
+            }
+            this.tail = newItem;
+        }
+
+        public int Dequeue()
+        {
+
+            int item = this.head.item;
+            head = head.next;
+            if (head == null)
+            {
+                tail = null;
+            }
+            return item;
+        }
+        public bool IsEmpty()
+        {
+            return head == null;
+        }
+
+    }
+
+}
