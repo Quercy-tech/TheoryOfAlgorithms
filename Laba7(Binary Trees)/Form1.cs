277473a14160c1a6cc2ad924cba9dd38495bc0c4
Alex <alexghezha@gmail.com>
2024-04-22 17:11:59 +0300

    Laba7

---
+++ Laba7(Binary Trees)/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Laba7_Binary_Trees_
+{
+    //public abstract
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // var bst = new BinarySearchTree();
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            bst.Insert(2);
+            bst.Insert(5);
+            bst.Insert(7);
+            bst.Insert(2);
+            bst.Insert(6);
+            bst.Insert(11);
+            bst.Insert(5);
+            bst.Insert(9);
+            bst.Insert(4);
+            stopwatch.Stop();
+            MessageBox.Show("Це зайняло " + stopwatch.ElapsedTicks + " ticks");
+        }
+
+        public BinarySearchTree bst = new BinarySearchTree();
+         public static string text = "";
+
+        public class Node
+        {
+            public int Power { get; set; }
+            public Node Left { get; set; }
+            public Node Right { get; set; }
+
+            public Node(int power)
+            {
+                Power = power;
+                Left = null;
+                Right = null;
+            }
+        }
+
+        public class BinarySearchTree
+        {
+            private Node root;
+
+            public void Insert(int power)
+            {
+                root = Insert(root, power);
+            }
+
+            private Node Insert(Node node, int power)
+            {
+                if (node == null)
+                {
+                    text += $"Creating a device with {power} power";
+                    return Insert(new Node(power), power);
+                }
+
+                if (power < node.Power)
+                {
+                    text += $"<- {node.Power}";
+                    node.Left = Insert(node.Left, power);
+                }
+                else if (power > node.Power)
+                {
+                    text += $"{node.Power} -> ";
+                    node.Right = Insert(node.Right, power);
+                }
+
+                return node;
+            }
+
+            public Node Find(int power)
+            {
+                return Find(root, power);
+            }
+
+            private Node Find(Node node, int power)
+            {
+                if (node == null)
+                {
+                    text += $"No device found, creating a new device with power {power}  \n";
+                    return Insert(node, power);
+
+                }
+
+                if (node.Power == power)
+                {
+                    text += $"Device with power {power} found";
+                    return node;
+                }
+
+
+
+                if (power < node.Power)
+                {
+                    text += $"<- {node.Power}";
+                    return Find(node.Left, power);
+                }
+                else
+                {
+                    text += $"{node.Power} -> ";
+                    return Find(node.Right, power);
+                }
+            }
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            text = "";
+
+            if (!int.TryParse(textBox1.Text, out int power) || power <= 0)
+            {
+                MessageBox.Show("Enter a valid input");
+            }
+            else
+            {
+                bst.Insert(power);
+                MessageBox.Show(text);
+            }
+            
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            text = "";
+
+            if (!int.TryParse(textBox1.Text, out int power) || power <= 0)
+            {
+                MessageBox.Show("Enter a valid input");
+            }
+            else
+            {
+                bst.Find(power);
+                MessageBox.Show(text);
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Laba7Console/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace Laba7Console
+{
+    class Node
+    {
+        public int Power { get; set; }
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+
+        public Node(int power)
+        {
+            Power = power;
+            Left = null;
+            Right = null;
+        }
+    }
+
+    class BinarySearchTree
+    {
+        private Node root;
+
+        public void Insert(int power)
+        {
+            Console.WriteLine($"root : {root?.Power}");
+            root = Insert(root, power);
+        }
+
+        private Node Insert(Node node, int power)
+        {
+            if (node == null)
+            {
+                Console.WriteLine($"Creating a device with {power} power");
+                return Insert(new Node(power), power);
+            }
+
+            if (power < node.Power)
+            {
+                Console.WriteLine($"<- {node.Power}");
+                node.Left = Insert(node.Left, power);
+            }
+            else if (power > node.Power)
+            {
+                Console.WriteLine($"{node.Power} -> ");
+                node.Right = Insert(node.Right, power);
+            }
+
+            return node;
+        }
+
+        public Node Find(int power)
+        {
+            return Find(root, power);
+        }
+
+        private Node Find(Node node, int power)
+        {
+            if (node == null)
+            {
+                Console.WriteLine($"No device found, creating a new device with power {power}");
+                return Insert(node,power);
+                
+            }
+
+            if (node.Power == power)
+            {
+                Console.WriteLine($"Device with power {power} found");
+                return node;
+            }
+
+            
+
+            if (power < node.Power)
+            {
+                Console.WriteLine($"<- {node.Power}");
+                return Find(node.Left, power);
+            }
+            else
+            {
+                Console.WriteLine($"{node.Power} -> ");
+                return Find(node.Right, power);
+            }
+        }
+
+
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var bst = new BinarySearchTree();
+
+            bst.Insert(1000);
+            bst.Insert(500);
+            bst.Insert(1500);
+
+            Console.WriteLine("Enter required power for the robot:");
+            int requiredPower = int.Parse(Console.ReadLine());
+
+            var device = bst.Find(requiredPower);
+
+
+            if (device != null) Console.WriteLine("Found suitable device with power: " + device.Power);
+
+        }
+    }
+}
